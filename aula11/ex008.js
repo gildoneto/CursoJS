f42d7d6474a88d1e2c070740247475e5456d4d7d
Gildo Neto <gildoneto@hotmail.com>
2021-11-14 05:30:40 -0300

    aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+console.log('teste')
+
+let vel = 12
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if(vel >= 60){ // Condição simples!
+    console.log(`Voce ultrapassou o limite. Multado!`)
+}
+console.log(`Dirija usando o cinto`)
